edcfdea9a3c964e5b6f9ba3b588dadd356d57e81
Aurélien Hervé <mail@aurelien-herve.com>
2015-03-11 11:07:27 +0100

    functional facebook login

---
+++ server/users.js
@@ -0,0 +1,6 @@
+//Meteor.publish("userData", function(){
+//  return Meteor.users.find({_id: this.userId}, {fields: {
+//    'services.facebook.first_name': 1
+//    'services.facebook.first_name': 1
+//  }})
+//});
