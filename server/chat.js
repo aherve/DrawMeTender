7f83d206921a6403635f857a825da8fca60df8ea
Aurélien Hervé <mail@aurelien-herve.com>
2015-03-11 11:13:53 +0100

    chat

--- server/chat.js
+++ server/chat.js
@@ -1,4 +1,17 @@
-  Meteor.publish("messages", function(){
-    return Messages.find({}, {sort: {createdAt: -1}, limit: 10});
-  })
+Meteor.publish("messages", function(){
+  return Messages.find({}, {sort: {createdAt: -1}, limit: 10});
+})
 
+Meteor.methods({
+  postMsg: function(text){
+
+    if (Meteor.user()){
+      Messages.insert({
+        text: text,
+      authorName: Meteor.user().profile.name,
+      authorId: Meteor.user()._id,
+      createdAt: new Date()
+      })
+    }
+  }
+})
